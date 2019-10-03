94cc4b697a460b64630ed32bfc9ff68f33429c10
Jason Huels, Neha Malhotra and Rachael Bouissey <rachael@>
2019-10-03 13:46:16 -0700

    add functionality to count upto hundred thousand

--- Numbers2Words.Tests/ModelTests/Numbers2WordsTests.cs
+++ Numbers2Words.Tests/ModelTests/Numbers2WordsTests.cs
@@ -14,7 +14,6 @@ namespace Numbers2Words.Tests
             int userInput = 9;
             string output = NumbersToWords.Numbers2Words(userInput);
 
-
             Assert.AreEqual(output, "nine");
         }
 
@@ -24,8 +23,48 @@ namespace Numbers2Words.Tests
             int userInput = 10;
             string output = NumbersToWords.Numbers2Words(userInput);
 
-
             Assert.AreEqual(output, "ten");
+        }
+        [TestMethod]
+         public void NumbersToWords_NinetyNine_NinetyNine()
+        {
+            int userInput = 99;
+            string output = NumbersToWords.Numbers2Words(userInput);
+
+            Assert.AreEqual(output, "ninety nine");
+        }
+
+        [TestMethod]
+         public void NumbersToWords_NinethousandnineHundreNinetyNine_NinethousandnineHundreNinetyNine()
+        {
+            int userInput = 9999;
+            string output = NumbersToWords.Numbers2Words(userInput);
+
+            Assert.AreEqual(output, "nine thousand nine hundred ninety nine");
+        }
+        [TestMethod]
+         public void NumbersToWords_NineteenthousandnineHundreNinetyNine_NineteenthousandnineHundreNinetyNine()
+        {
+            int userInput = 19999;
+            string output = NumbersToWords.Numbers2Words(userInput);
+
+            Assert.AreEqual(output, "nineteen thousand nine hundred ninety nine");
+        }
+        [TestMethod]
+         public void NumbersToWords_NinetyninethousandnineHundreNinetyNine_NinetythousandnineHundreNinetyNine()
+        {
+            int userInput = 99999;
+            string output = NumbersToWords.Numbers2Words(userInput);
+
+            Assert.AreEqual(output, "ninety nine thousand nine hundred ninety nine");
+        }
+        [TestMethod]
+         public void NumbersToWords_NineHundredNinetyninethousandnineHundreNinetyNine_NineHundredNinetythousandnineHundreNinetyNine()
+        {
+            int userInput = 999999;
+            string output = NumbersToWords.Numbers2Words(userInput);
+
+            Assert.AreEqual(output, "nine hundred ninety nine thousand nine hundred ninety nine");
         }
     }
 
--- Numbers2Words/Models/Numbers2Words.cs
+++ Numbers2Words/Models/Numbers2Words.cs
@@ -7,21 +7,50 @@ namespace Numbers2Words
         public static string Numbers2Words(int number)
         {
             string output = "";
+            int ud = 48; // unicode value for 0, used to change unicode value into integer
             string[] ones = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
             string[] teens = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
             string[] tens = {"dummy", "dummy", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", " eighty", "ninety"};
             string[] place = {"dummy", "dummy", "hundred", "thousand", "million", "billion", "trillion"};
-            
+
             if(number > 19)
             {
                 char[] nums = number.ToString().ToCharArray();
                 int[] numArr = new int[nums.Length];
                 for(int i=0; i<nums.Length; i++)
                 {
-                    numArr[i] = ((int)nums[nums.Length-1-i])-48;
+                    numArr[i] = ((int)nums[nums.Length-1-i])-ud;
                 } 
+                if(number >= 999999)
+                {
+                    output += ones[numArr[5]] + " " + place[2] + " ";
+                    number -= numArr[5] * (int)Math.Pow(10, 5);
+                }
+                if(number > 19999)
+                {
+                    output += tens[numArr[4]] + " ";
+                    number -= numArr[4] * (int)Math.Pow(10, 4);
+                }
+                if (number > 9999) 
+                {
+                    output += teens[numArr[3]] + " " + place[3] + " ";
+                    number -= numArr[3] * (int)Math.Pow(10, 3) + numArr[4] * (int)Math.Pow(10, 4);
+                }
+                if (number > 999)
+                {
+                    output += ones[numArr[3]] + " " + place[3] + " ";
+                    number -= numArr[3] * (int)Math.Pow(10, 3);
+                }
+
+                if(number > 99)
+                {
+                    output += ones[numArr[2]] + " " + place[2] + " ";
+                    number -= numArr[2] * (int)Math.Pow(10, 2);
+                }
+                output += tens[numArr[1]] + " ";
+                number -= numArr[1] * (int)Math.Pow(10, 1); 
+
             }
-            
             
             if(number > 9 && number < 20)
             {
@@ -29,7 +58,8 @@ namespace Numbers2Words
             }
             else
             {
-                output = ones[number];
+                Console.WriteLine(number);
+                output += ones[number];
             }
             return output;
         }
